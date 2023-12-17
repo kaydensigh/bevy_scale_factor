ed817d7e424226ec64ffe54f698301b3e97a35a9
Jack Hou <jack.m.hou@gmail.com>
2023-12-17 11:44:26 +0100

    Initial commit.
    Cut down to a minimal example and update README.md.
    Add viewport meta to index.html to fix mobile scaling.
    Add Trunk.toml to make paths relative; enables IDE live preview which serves the whole repo.

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,138 @@
-// disable console on windows for release builds
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 use bevy::prelude::*;
-use bevy::window::PrimaryWindow;
-use bevy::winit::WinitWindows;
-use bevy::DefaultPlugins;
-use bevy_game::GamePlugin; // ToDo: Replace bevy_game with your new crate name.
-use std::io::Cursor;
-use bevy::asset::AssetMetaCheck;
-use winit::window::Icon;
+use bevy::render::settings::Backends;
+use bevy::render::settings::RenderCreation;
+use bevy::render::settings::WgpuSettings;
+use bevy::render::RenderPlugin;
 
 fn main() {
-    App::new()
-        .insert_resource(Msaa::Off)
-        .insert_resource(AssetMetaCheck::Never)
-        .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
+    let mut app = App::new();
+    if cfg!(target_family = "wasm") {
+        app.add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                title: "Bevy game".to_string(), // ToDo
-                // Bind to canvas included in `index.html`
                 canvas: Some("#bevy".to_owned()),
-                // The canvas size is constrained in index.html and build/web/styles.css
                 fit_canvas_to_parent: true,
-                // Tells wasm not to override default event handling, like F5 and Ctrl+R
-                prevent_default_event_handling: false,
+                prevent_default_event_handling: true,
                 ..default()
             }),
             ..default()
-        }))
-        .add_plugins(GamePlugin)
-        .add_systems(Startup, set_window_icon)
-        .run();
+        }));
+    } else if cfg!(target_family = "windows") {
+        app.add_plugins(DefaultPlugins.set(RenderPlugin {
+            render_creation: RenderCreation::Automatic(WgpuSettings {
+                backends: Some(Backends::DX12),
+                ..default()
+            }),
+        }));
+    } else {
+        app.add_plugins(DefaultPlugins);
+    }
+
+    app.add_systems(Startup, setup);
+    app.add_systems(
+        Update,
+        (
+            update_ui,
+            update_window,
+            update_viewport,
+            update_mouse,
+            update_touch,
+        ),
+    );
+
+    app.run();
 }
 
-// Sets the icon on windows and X11
-fn set_window_icon(
-    windows: NonSend<WinitWindows>,
-    primary_window: Query<Entity, With<PrimaryWindow>>,
+#[derive(Component)]
+struct WholeScreenNode;
+#[derive(Component)]
+struct UiDetails;
+#[derive(Component)]
+struct WindowDetails;
+#[derive(Component)]
+struct ViewportDetails;
+#[derive(Component)]
+struct MouseDetails;
+#[derive(Component)]
+struct TouchDetails;
+
+fn setup(
+    mut commands: Commands,
 ) {
-    let primary_entity = primary_window.single();
-    let Some(primary) = windows.get_window(primary_entity) else {
-        return;
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    flex_direction: FlexDirection::Column,
+                    ..default()
+                },
+                ..default()
+            },
+            WholeScreenNode,
+        ))
+        .with_children(|parent| {
+            parent.spawn((text_bundle(), UiDetails));
+            parent.spawn((text_bundle(), WindowDetails));
+            parent.spawn((text_bundle(), ViewportDetails));
+            parent.spawn((text_bundle(), MouseDetails));
+            parent.spawn((text_bundle(), TouchDetails));
+        });
+
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn text_bundle() -> TextBundle {
+    TextBundle::from_section(
+        "",
+        TextStyle {
+            font_size: 30.,
+            ..default()
+        },
+    )
+}
+
+fn update_ui(
+    ui_scale: Res<UiScale>,
+    whole_screen_node: Query<(&Node, &GlobalTransform), With<WholeScreenNode>>,
+    mut ui_text: Query<&mut Text, With<UiDetails>>,
+) {
+    let (wsn, wsngt) = whole_screen_node.single();
+    ui_text.single_mut().sections[0].value = format!(
+        "UI scale {}, UI rect {:?}",
+        ui_scale.0,
+        wsn.logical_rect(wsngt)
+    );
+}
+fn update_window(window: Query<&Window>, mut window_text: Query<&mut Text, With<WindowDetails>>) {
+    let w = window.single();
+    window_text.single_mut().sections[0].value = format!(
+        "Window scale {} logical {} {}, physical {} {}",
+        w.scale_factor(),
+        w.resolution.width(),
+        w.resolution.height(),
+        w.resolution.physical_width(),
+        w.resolution.physical_height()
+    );
+}
+fn update_viewport(
+    camera: Query<&Camera>,
+    mut viewport_text: Query<&mut Text, With<ViewportDetails>>,
+) {
+    let c = camera.single();
+    viewport_text.single_mut().sections[0].value = format!(
+        "Viewport logical {:?}, physical {:?}",
+        c.logical_viewport_rect(),
+        c.physical_viewport_rect()
+    );
+}
+fn update_mouse(window: Query<&Window>, mut mouse_text: Query<&mut Text, With<MouseDetails>>) {
+    if let Some(cursor) = window.single().cursor_position() {
+        mouse_text.single_mut().sections[0].value = format!("Mouse position: {}", cursor);
     };
-    let icon_buf = Cursor::new(include_bytes!(
-        "../build/macos/AppIcon.iconset/icon_256x256.png"
-    ));
-    if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
-        let image = image.into_rgba8();
-        let (width, height) = image.dimensions();
-        let rgba = image.into_raw();
-        let icon = Icon::from_rgba(rgba, width, height).unwrap();
-        primary.set_window_icon(Some(icon));
-    };
+}
+fn update_touch(touches: Res<Touches>, mut touch_text: Query<&mut Text, With<TouchDetails>>) {
+    for t in touches.iter() {
+        touch_text.single_mut().sections[0].value = format!("Touch {}", t.position());
+    }
 }
